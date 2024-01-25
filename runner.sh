3abbad7032512b78d3f0a3c31b83f04b8093b026
Filip Urban <urban-filip@outlook.com>
2024-01-25 19:34:24 +0100

    formatted runner

--- runner.sh
+++ runner.sh
@@ -19,14 +19,12 @@ echo "-------------------------------------------"
 # Do not start the tests immediately. Hub has to be ready with browser nodes
 echo "Checking if hub is ready..!"
 count=0
-while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-do
-  count=$((count+1))
+while [ "$(curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready)" != "true" ]; do
+  count=$((count + 1))
   echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ]
-  then
-      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
-      exit 1
+  if [ "$count" -ge 30 ]; then
+    echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+    exit 1
   fi
   sleep 1
 done
@@ -36,9 +34,9 @@ echo "Selenium Grid is up and running. Running the test...."
 
 # Start the java command
 java -cp 'libs/*' \
-     -Dselenium.grid.enabled=true \
-     -Dselenium.grid.hubHost="${HUB_HOST:-hub}" \
-     -Dbrowser="${BROWSER:-chrome}" \
-     org.testng.TestNG \
-     -threadcount "${THREAD_COUNT:-1}" \
-     test-suites/"${TEST_SUITE}"
+  -Dselenium.grid.enabled=true \
+  -Dselenium.grid.hubHost="${HUB_HOST:-hub}" \
+  -Dbrowser="${BROWSER:-chrome}" \
+  org.testng.TestNG \
+  -threadcount "${THREAD_COUNT:-1}" \
+  test-suites/"${TEST_SUITE}"
